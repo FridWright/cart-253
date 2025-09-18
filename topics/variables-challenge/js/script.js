47f3e5481bb96b1b6c29ed7ccba27be0d2502d49
FridWright <fridwright@yahoo.ca>
2025-09-18 16:48:15 -0400

    updated mr furious project

--- topics/variables-challenge/js/script.js
+++ topics/variables-challenge/js/script.js
@@ -15,10 +15,32 @@ let mrFurious = {
   size: 100,
   // Colour
   fill: {
-    r: 255,
-    g: 225,
-    b: 225
+        r: 255,
+        g: 225,
+        b: 225
   }
+};
+
+let sky = {
+    fill: {
+        r: 160,
+        g: 180,
+        b: 200
+    }
+};
+
+let bird = {
+    x: 0,
+    y: 80,
+    size: 20,
+    fill: {
+        r: 255,
+        g: 105,
+        b: 180
+    },
+    velocity: {
+        x: 1.5
+    }
 };
 
 /**
@@ -32,7 +54,7 @@ function setup() {
  * Draw (and update) Mr. Furious
  */
 function draw() {
-  background(160, 180, 200);
+  background(sky.fill.r, sky.fill.g, sky.fill.b);
   
   // Draw Mr. Furious as a coloured circle
   push();
@@ -40,4 +62,29 @@ function draw() {
   fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
   ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
   pop();
+
+  // Change turn red over time
+  mrFurious.fill.g = mrFurious.fill.g - 0.5;
+  mrFurious.fill.b = mrFurious.fill.b - 0.5;
+
+  // Change sky from blue to black over time
+  sky.fill.r = sky.fill.r - 1;
+  sky.fill.g = sky.fill.g - 1;
+  sky.fill.b = sky.fill.b - 1;
+
+  // Drawing a bird
+  push();
+  noStroke();
+  fill(bird.fill.r, bird.fill.g, bird.fill.b);
+  ellipse(bird.x, bird.y, bird.size);
+  pop();
+
+  // Moving the bird
+  bird.x = bird.x + bird.velocity.x;
+
+  // Make Mr. Furious shake and become bigger
+  mrFurious.x = mrFurious.x + random(-2, 2);
+  mrFurious.y = mrFurious.y + random(-2, 2);
+  mrFurious.size = mrFurious.size + 0.5;
+
 }
